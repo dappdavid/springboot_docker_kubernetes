41732e723b6705d205705bf9dcec1166ecf6512c
dappdavid <dappdavid99@gmail.com>
2022-10-07 22:01:35 +0530

    first

---
+++ src/main/java/com/springboot_docker_kubernetes/SpringbootDockerKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_docker_kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerKubernetesApplication.class, args);
+	}
+
+}
